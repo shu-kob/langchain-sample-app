cbf003cf2c0af40ee47fac329b352315e37020b0
小渕周 <shukobuchi@gmail.com>
2024-03-23 23:46:26 +0900

    質問返答ができるように

--- sample_app.py
+++ sample_app.py
@@ -1,10 +1,20 @@
 import chainlit as cl
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
+
+chat = ChatOpenAI(
+  model="gpt-3.5-turbo",
+)
 
 @cl.on_chat_start
 async def on_chat_start():
-  await cl.Message(content="Hello, I am a sample app. How can I help you?").send()
+  await cl.Message(content="こんにちは。私はAIです。質問をしてください。").send()
 
 @cl.on_message
 async def on_message(input_message):
   print("Received message: ", input_message)
-  await cl.Message(content="You said: " + input_message).send()
+
+  result = chat([
+    HumanMessage(content=input_message),
+  ])
+  await cl.Message(content=result.content).send()
